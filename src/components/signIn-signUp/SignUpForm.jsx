7ded09853ab7273c24f1b5f1b7a8dae890133cc9
shady bassily <shadybassily@yahoo.com>
2023-01-31 17:05:18 +0200

    remove (re-directing the user if they're authenticated) from signUp form

--- src/components/signIn-signUp/SignUpForm.jsx
+++ src/components/signIn-signUp/SignUpForm.jsx
@@ -1,56 +1,39 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import AnimatingBtn from "../animating-btn/AnimatingBtn";
 import "./forms.css";
 import ThirdPartySignUp from "./ThirdPartySignUp";
-
-
-
+const fields = [
+  {
+    label: "Clinic's owner name",
+    placeholder: "Ex: mahmoud barbary",
+    type: "text",
+  },
+  {
+    label: "Clinic's full name",
+    placeholder: "Ex: Twoth london",
+    type: "text",
+  },
+  {
+    label: "Clinic's email",
+    placeholder: "Ex: twoth@example",
+    type: "text",
+  },
+  {
+    label: "Clinic's mobile number",
+    placeholder: "Ex: +201061949054",
+    type: "text",
+  },
+  {
+    label: "Password",
+    placeholder: "****************",
+    type: "password",
+  },
+];
 
 export default function SignUpForm() {
-  const navigate = useNavigate();
-  const user = useSelector((state) => state.user.user);
-  console.log(user);
-  //if the user is authenticated, redirect them to generate plan
-  useEffect(() => {
-    if (user.isAuth) {
-      navigate("/generate-plan");
-    }
-  });
-
-  const fields = [
-    {
-      label: "Clinic's owner name",
-      placeholder: "Ex: mahmoud barbary",
-      type: "text",
-    },
-    {
-      label: "Clinic's full name",
-      placeholder: "Ex: Twoth london",
-      type: "text",
-    },
-    {
-      label: "Clinic's email",
-      placeholder: "Ex: twoth@example",
-      type: "text",
-    },
-    {
-      label: "Clinic's mobile number",
-      placeholder: "Ex: +201061949054",
-      type: "text",
-    },
-    {
-      label: "Password",
-      placeholder: "****************",
-      type: "password",
-    },
-  ];
-
-  const handleSubmit = () => {};
   return (
     <div className="sign-form-container">
-      <h2 className="sign-up-header">Create a Clinic Account</h2>
+      <h2 className="form-header">Create a Clinic Account</h2>
       <form className="sign-form">
         {fields.map((f, i) => (
           <div className="field-container" key={i}>
@@ -60,10 +43,10 @@ export default function SignUpForm() {
         ))}
         <AnimatingBtn className="submit">Create account</AnimatingBtn>
       </form>
+      
       <div className="third-party-auth">
         -OR-
         <ThirdPartySignUp />
-
         <small>
           Already have an account?{" "}
           <Link to="/" className="link">
